58c56a77584d15a1fb136cdf49ad6560aaf85db2
kevduffy <kevrobduffy@gmail.com>
2017-11-11 14:46:28 -0500

    Update project.py

--- project/project.py
+++ project/project.py
@@ -3,13 +3,16 @@ df = pd.read_csv('federalelections2012.csv', names=['state', 'district', 'lastna
 
 #need to insert a way to add statewide vote totals for each party
 =SUMPRODUCT(SUMIF(state:state;List;B:B))
-                                                    
+
+if 'party'="R" then 'votes'="rvotes"
+  elsif 'party'="D" the 'votes'="dvotes"                                                    
  #determining the state winner determines the formula we use to find the wasted votes                                                   
-if "rvotes">"dvotes" then winner=="rep" 
-  elsif dvotes>rvotes then winner=dem
+for district 
+  if "rvotes">"dvotes" then winner="rep" 
+  elsif dvotes>rvotes then winner="dem"
                                                     
 #determining number of wasted votes
-if winner=rep then rwaste=rvotes-(dvotes+1)
+if winner="rep" then rwaste=rvotes-(dvotes+1)
   dwaste=dvotes
 elsif winner=dem then dwaste=dvotes-(rvotes+1)
   rwaste=rvotes
@@ -20,7 +23,7 @@ EG=(rwaste-dwaste)/rvotes+dvotes
 #determining republican or democrat efficiency gap advantage                                                    
 if EG>0 then advantage="democrat"
   elsif EG<0 then advantage="republican"
-  elsif EG=0 then advantage="no advantage"
+  elsif EG==0 then advantage="no advantage"
                                                     
 #determining if efficiency gap is dire enough to constitute illegal gerrymandering
 if abs(EG)>=.07
