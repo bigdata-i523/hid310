b2c926c3196531b3bc3aece694c2552a743bd966
kevduffy <kevrobduffy@gmail.com>
2017-11-14 22:25:55 -0500

    Update project2.py

--- project/project2.py
+++ project/project2.py
@@ -1,21 +1,13 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('federalelections2012.csv')
-    state=df.state
-    district=df.district
-    lastname=df.lastname
-    party=df.party
-    votes=df.votes   
-
-def state(state)
-                 
-def votes(votes)
+import csv
+data = np.genfromtxt("federalelections2012.csv", dtype=None, delimiter=',', names=True, skip_header=0) 
                  
 def wastedvotes(
-    if state=state and district=district:
-        max(votes)
-            if max(party="R"):
-                (votes("D")+(votes("R")-(votes("D")+1)))
+    if data['state']=data['state'] and data['district']=data['district']:
+       find max(data['votes'])
+            if max(data['party']="R"):
+                for data['votes', 'party'](data['votes'="D"]+(data['votes'="R")-(data['votes'="D"]+1)))
             else (votes("R")+(votes("D")-(votes("R")+1))))
                  
 def eg(
