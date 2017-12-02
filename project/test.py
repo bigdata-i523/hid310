33cca488252a3b4ab49620c1cb2f36de00ec5c15
kevduffy <kevrobduffy@gmail.com>
2017-12-02 01:57:33 -0500

    Update test.py

--- project/test.py
+++ project/test.py
@@ -1,25 +1,24 @@
-import numpy as np
 import pandas as pd
-import csv
 
 df = pd.read_csv('house.csv')
 
-def dwaste():
+def dwaste(row):
     if row['dvotes'] > row['rvotes']:
         val = row['dvotes'] - ((row['dvotes'] + row['rvotes']) * .5)
     else: 
         val = row['dvotes']
     return val
         
-def rwaste():
-    if row['rvotes'] > row['dvotes']
+def rwaste(row):
+    if row['rvotes'] > row['dvotes']:
         val = row['rvotes'] - ((row['dvotes'] + row['rvotes']) * .5)
     else:
         val = row['rvotes']
     return val
         
-def gap():
-    val = (df.apply(rwaste) - df.apply(dwaste))/(df['dvotes']+df['rvotes'])
-    print(val)
-    
-gap    
+df['rwaste'] = df.apply(rwaste, axis=1)
+df['dwaste'] = df.apply(dwaste, axis=1)
+
+print("The efficiency gap score for Indiana's state house district map is:")
+print((df['dwaste'].sum() - df['rwaste'].sum())/(df['rvotes'].sum()+df['dvotes'].sum())
+print("This does not meet the threshold for illegal gerrymandering.")
