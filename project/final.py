05b60bea4bf8e7d447e3d52296677ca4e68bace0
kevduffy <kevrobduffy@gmail.com>
2017-12-02 02:11:29 -0500

    Update and rename test.py to final.py

--- project/final.py
+++ project/final.py
@@ -8,17 +8,21 @@ def dwaste(row):
     else: 
         val = row['dvotes']
     return val
-        
+
 def rwaste(row):
     if row['rvotes'] > row['dvotes']:
         val = row['rvotes'] - ((row['dvotes'] + row['rvotes']) * .5)
     else:
         val = row['rvotes']
     return val
-        
+
 df['rwaste'] = df.apply(rwaste, axis=1)
 df['dwaste'] = df.apply(dwaste, axis=1)
 
-print("The efficiency gap score for Indiana's state house district map is:")
-print((df['dwaste'].sum() - df['rwaste'].sum())/(df['rvotes'].sum()+df['dvotes'].sum())
-print("This does not meet the threshold for illegal gerrymandering.")
+rtotal = df['rwaste'].sum()
+dtotal = df['dwaste'].sum()
+bottom = df['rvotes'].sum() + df['dvotes'].sum()
+
+print("The efficiency gap score for the Indiana state house district map is:")
+print((dtotal-rtotal)/bottom)
+print("This does not reach the threshold for illegal gerrymandering.")
