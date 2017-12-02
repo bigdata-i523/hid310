9a39ec30123b838d914ff535d20078f4ffbc10f1
kevduffy <kevrobduffy@gmail.com>
2017-12-02 08:40:48 -0500

    Update final.py

--- project/final.py
+++ project/final.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('house.csv')
+df2 = pd.read_csv('senate.csv')
 #The efficiency gap is the wasted votes from party a minus those from party b, divided by the total number of votes. A wasted vote is any vote cast for a losing candidate, or any vote for a winning candidate beyond the threshold needed to win.
 #Finding democrat wasted votes:
 def dwaste(row):
@@ -16,14 +17,25 @@ def rwaste(row):
     else:
         val = row['rvotes']
     return val
-#adding these values to our dataframe
+#adding these values to our dataframes
 df['rwaste'] = df.apply(rwaste, axis=1)
 df['dwaste'] = df.apply(dwaste, axis=1)
+
+df2['rwaste'] = df2.apply(rwaste, axis=1)
+df2['dwaste'] = df2.apply(dwaste, axis=1)
 #applying the efficiency gap formula
 rtotal = df['rwaste'].sum()
 dtotal = df['dwaste'].sum()
 bottom = df['rvotes'].sum() + df['dvotes'].sum()
+
+rtotal2 = df2['rwaste'].sum()
+dtotal2 = df2['dwaste'].sum()
+bottom2 = df2['rvotes'].sum() + df2['dvotes'].sum()
 #The result
-print("The efficiency gap score for the Indiana state house district map is:")
+print("The efficiency gap score for the Indiana state HOUSE district map is:")
 print((dtotal-rtotal)/bottom)
+print("This does not reach the threshold for illegal gerrymandering.")
+print(" ")
+print("The efficiency gap score for the Indiana state SENATE district map is:")
+print((dtotal2-rtotal2)/bottom2)
 print("This does not reach the threshold for illegal gerrymandering.")
